c23fa34375e05df57952444c7ab1bb2579e9dd9c
fcieslik <frederic.cieslik@t-sytems.com>
2019-06-24 08:08:57 +0200

    Weiteres Beispiel hinzugefügt

--- src/kmeans_demo.py
+++ src/kmeans_demo.py
@@ -7,28 +7,61 @@ from sklearn.datasets import make_moons
 
 plt.figure()
 
+# Beispiel 1
 n_samples = 300
-X, y = make_blobs(n_samples=n_samples, centers=2)
-y_pred = KMeans(n_clusters=2).fit_predict(X)
+X, y = make_blobs(n_samples=n_samples, centers=3, random_state=200)
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.title("Erstes Beispiel")
+plt.show()
+
+y_pred = KMeans(n_clusters=3).fit_predict(X)
 
 plt.scatter(X[:, 0], X[:, 1], c=y_pred)
 plt.title("Erstes Beispiel")
 plt.show()
 
-# Nummer 2
+# Beispiel 2
 n_samples = 300
-X, y = make_blobs(n_samples=n_samples, centers=2)
-y_pred = KMeans(n_clusters=3).fit_predict(X)
+X, y = make_blobs(n_samples=n_samples, centers=3, random_state=100)
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.title("Zweites Beispiel")
+plt.show()
+
+y_pred = KMeans(n_clusters=4).fit_predict(X)
 
 plt.scatter(X[:, 0], X[:, 1], c=y_pred)
 plt.title("Zweites Beispiel")
 plt.show()
 
-# Nummer 3
+
+# Beispiel 3
 n_samples = 300
-X, y = make_moons(200, noise=.05, random_state=0)
+X, y = make_moons(n_samples=n_samples, noise=.05, random_state=0)
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.title("Drittes Beispiel")
+plt.show()
+
 y_pred = KMeans(n_clusters=2).fit_predict(X)
 
 plt.scatter(X[:, 0], X[:, 1], c=y_pred)
 plt.title("Drittes Beispiel")
+plt.show()
+
+# Beispiel 4
+n_samples = 300
+X, y = make_blobs(n_samples=n_samples, centers=3, random_state=170)
+transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
+X = np.dot(X, transformation)
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.title("Viertes Beispiel")
+plt.show()
+
+y_pred = KMeans(n_clusters=3).fit_predict(X)
+
+plt.scatter(X[:, 0], X[:, 1], c=y_pred)
+plt.title("Viertes Beispiel")
 plt.show()
